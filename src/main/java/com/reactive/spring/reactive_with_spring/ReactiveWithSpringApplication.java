ac5bc04b9ea117384c7e47896954373ebfb506eb
P173073 <deed1515@naver.com>
2024-08-12 18:01:46 +0900

    :tada: [init] WebFlux!!

---
+++ src/main/java/com/reactive/spring/reactive_with_spring/ReactiveWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.spring.reactive_with_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reactive/spring/reactive_with_spring/ReactiveWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.spring.reactive_with_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
