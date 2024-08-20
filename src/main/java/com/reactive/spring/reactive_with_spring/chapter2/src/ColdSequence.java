e50bc45fe0610512f88d9f65757cdab409fc0edc
P173073 <deed1515@naver.com>
2024-08-20 09:54:57 +0900

    :sparkles: Hot Sequence Cold Sequence  - share()

--- src/main/java/com/reactive/spring/reactive_with_spring/chapter2/src/ColdSequence.java
+++ src/main/java/com/reactive/spring/reactive_with_spring/chapter2/src/ColdSequence.java
@@ -1,4 +1,20 @@
 package com.reactive.spring.reactive_with_spring.chapter2.src;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+
+@Slf4j
 public class ColdSequence {
+    public static void main(String[] args) throws InterruptedException {
+        Flux<String> coldFlux = Flux.fromIterable(Arrays.asList("KOREA","JAPAN","CHINA"))
+                .map(String::toLowerCase);
+
+        coldFlux.subscribe(country -> System.out.println("country : "+ country));
+        System.out.println("----------------------------------");
+
+        Thread.sleep(2000L);
+        coldFlux.subscribe(country -> System.out.println("country 2: "+ country));
+    }
 }
--- src/main/java/com/reactive/spring/reactive_with_spring/chapter2/src/HotSequence.java
+++ src/main/java/com/reactive/spring/reactive_with_spring/chapter2/src/HotSequence.java
@@ -1,4 +1,21 @@
 package com.reactive.spring.reactive_with_spring.chapter2.src;
 
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
 public class HotSequence {
+    public static void main(String[] args) throws InterruptedException {
+        String[] singer = {"Singer A","Singer B","Singer C","Singer D","Singer E","Singer F"};
+
+        Flux<String> conertFlux = Flux.fromArray(singer)
+                .delayElements(Duration.ofSeconds(1))
+                .share();
+
+        conertFlux.subscribe( singger -> System.out.println("Subscribe " + singger + "`s song"));
+        Thread.sleep(2000);
+
+        conertFlux.subscribe( singger -> System.out.println("Subscribe 2 " + singger + "`s song"));
+        Thread.sleep(5000);
+    }
 }
