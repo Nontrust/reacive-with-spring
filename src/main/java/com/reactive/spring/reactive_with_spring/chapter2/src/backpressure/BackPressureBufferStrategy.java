f7a735fcd25678e38ebf8426295cb0103b37bb03
P173073 <deed1515@naver.com>
2024-08-29 18:19:12 +0900

    :sparkles: Back Pressure  - 전략 drop oldest <-> drop latest

--- src/main/java/com/reactive/spring/reactive_with_spring/chapter2/src/backpressure/BackPressureBufferStrategy.java
+++ src/main/java/com/reactive/spring/reactive_with_spring/chapter2/src/backpressure/BackPressureBufferStrategy.java
@@ -14,7 +14,8 @@ public class BackPressureBufferStrategy {
                 // buffer 내부 over flow 를 만든 데이터를 drop
                 .onBackpressureBuffer(2,
                         drop -> System.out.println("over Flow & drop" + drop),
-                        BufferOverflowStrategy.DROP_LATEST)
+                        BufferOverflowStrategy.DROP_OLDEST) // buffer에 처음 들어온 데이터 제거
+//                        BufferOverflowStrategy.DROP_LATEST) // buffer에 마지막에 들어온 데이터 제거
                 .doOnNext(data -> System.out.println("emmit by buffer" + data))
                 .publishOn(Schedulers.parallel(), false, 1)
                 .subscribe(data -> {
